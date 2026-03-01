db2960b7218c9a465ea1baf23bc0f300a0b78a28
newrising2007-hue <newrising2007@gmail.com>
2026-03-01 09:33:47 -0500

    feat: lien Dev visible seulement en local

--- components/LayoutChrome.tsx
+++ components/LayoutChrome.tsx
@@ -142,6 +142,12 @@ export default function LayoutChrome({
                 Codes bonus
               </Link>
 
+              {process.env.NEXT_PUBLIC_DEV_MODE === "true" && (
+                <Link href="/test-affiliate" className="rounded-lg px-2 py-1 text-orange-500 hover:bg-orange-50 font-semibold">
+                  🔧 Dev
+                </Link>
+              )}
+
               <LanguageSwitch />
 
               <Link
